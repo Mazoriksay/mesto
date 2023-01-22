105380d86a684799ac93d24eda777207b164d20d
Nikita Kovalev <mazoriksay.aod@gmail.com>
2023-01-23 00:40:05 +0300

    feat: Добавление скриптов

---
+++ script.js
@@ -0,0 +1,31 @@
+function popupActive() {
+    let popup = document.querySelector('.popup');
+    popup.classList.remove('popup_status_close');
+}
+
+function popupClose() {
+    let popup = document.querySelector('.popup');
+    popup.classList.add('popup_status_close');
+}
+
+let editButton = document.querySelector('.profile__edit');
+editButton.addEventListener('click', popupActive);
+
+let closeButton = document.querySelector('.popup__button-close');
+closeButton.addEventListener('click', popupClose);
+
+let formElement = document.querySelector('form');
+let nameInput = document.getElementById('name');
+let aboutInput = document.getElementById('about');
+let nameProfile = document.querySelector('.profile__name');
+let aboutProfile = document.querySelector('.profile__about');
+nameInput.setAttribute('value', nameProfile.textContent);
+aboutInput.setAttribute('value', aboutProfile.textContent);
+
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+    nameProfile.textContent = `${nameInput.value}`;
+    aboutProfile.textContent = `${aboutInput.value}`;
+}
+formElement.addEventListener('submit', handleFormSubmit);
+formElement.addEventListener('submit', popupClose);
