14cb1ace0c5b2c70b79f9ab2f2cba6c2f094a2fe
Nikita Kovalev <mazoriksay.aod@gmail.com>
2023-01-24 01:29:12 +0300

    fix: Исправить попап и стили

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,11 @@
+let popup = document.querySelector('.popup');
 function popupActive() {
-    let popup = document.querySelector('.popup');
     popup.classList.add('popup_opened');
+    nameInput.setAttribute('value', nameProfile.textContent);
+    aboutInput.setAttribute('value', aboutProfile.textContent);
 }
 
 function popupClose() {
-    let popup = document.querySelector('.popup');
     popup.classList.remove('popup_opened');
 }
 
@@ -14,18 +15,16 @@ editButton.addEventListener('click', popupActive);
 let closeButton = document.querySelector('.popup__button-close');
 closeButton.addEventListener('click', popupClose);
 
-let formElement = document.querySelector('form');
+let formElement = document.querySelector('.popup__form');
 let nameInput = document.getElementById('name');
 let aboutInput = document.getElementById('about');
 let nameProfile = document.querySelector('.profile__name');
 let aboutProfile = document.querySelector('.profile__about');
-nameInput.setAttribute('value', nameProfile.textContent);
-aboutInput.setAttribute('value', aboutProfile.textContent);
 
 function handleFormSubmit(evt) {
     evt.preventDefault();
     nameProfile.textContent = `${nameInput.value}`;
     aboutProfile.textContent = `${aboutInput.value}`;
+    popupClose()
 }
 formElement.addEventListener('submit', handleFormSubmit);
-formElement.addEventListener('submit', popupClose);
